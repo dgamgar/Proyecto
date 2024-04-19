5ba4721fe2fa74cde03a64b2e3083e59a112f963
daniel <dgamgar755a@ieslacampina.es>
2024-04-19 11:15:54 +0200

    pongo bonito solo ningún cambio importante

--- modificar-modelo.php
+++ modificar-modelo.php
@@ -60,55 +60,55 @@ if ($resultado->num_rows > 0) {
     </div>
     <?php
     } else {
-                // Sí lo ha traído
-                $nmarca=$_GET['id'];
-                // Establezco conexión con la BD
-                require "conexion.php";
-        
-                // Saco el ID de la marca para mostrar los modelos de esta
-                $sql="SELECT ID_marca FROM marca WHERE nombre_marca='$nmarca'";
-                $resultado=$mysqli->query($sql);
-        
-                while($fila=$resultado->fetch_assoc()){
-                    $id=$fila['ID_marca'];
-                }
-        
-                // Saco los datos de los modelos de la marca escogida
-                $sql1="SELECT * FROM modelo WHERE ID_marca='$id'";
-                $resultado1=$mysqli->query($sql1);
-                ?>
-                <div class="container">
-                    <!-- Tabla con los modelos -->
-                    <table id="tabla" class="display" >
-                                <thead>
-                                    <tr>
-                                        <th class="th">Modelo</th>
-                                        <th class="th">Número de puertas</th>
-                                        <th class="th">Tipo de combustible</th>
-                                        <th class="th">Potencia(CV)</th>
-                                        <th></th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <?php
-                                        while($fila1 = $resultado1->fetch_assoc()){
-                                            echo "<tr>";
-                                            echo "<td class='td'>$fila1[nombre_modelo]</td>";
-                                            echo "<td class='td'>$fila1[num_puertas]</td>";
-                                            echo "<td class='td'>$fila1[combustible]</td>";
-                                            echo "<td class='td'>$fila1[cv]</td>";
-                                            ?>
-                                            <!-- Guardo el ID del modelo escogido para llevarlo a la otra página-->
-                                            <td><a href="modificar-modelo2.php?id=<?php echo $fila1['ID_modelo'];?>">Modificar modelo</a></td>
-                                            <?php
-                                            echo "</tr>";
-                                        }
+        // Guardo el nombre de la marca en una variable
+        $nmarca=$_GET['id'];
+        // Establezco conexión con la BD
+        require "conexion.php";
+
+        // Saco el ID de la marca para mostrar los modelos de esta
+        $sql="SELECT ID_marca FROM marca WHERE nombre_marca='$nmarca'";
+        $resultado=$mysqli->query($sql);
+
+        while($fila=$resultado->fetch_assoc()){
+            $id=$fila['ID_marca'];
+        }
+
+        // Saco los datos de los modelos de la marca escogida
+        $sql1="SELECT * FROM modelo WHERE ID_marca='$id'";
+        $resultado1=$mysqli->query($sql1);
+        ?>
+        <div class="container">
+            <!-- Tabla con los modelos -->
+            <table id="tabla" class="display" >
+                        <thead>
+                            <tr>
+                                <th class="th">Modelo</th>
+                                <th class="th">Número de puertas</th>
+                                <th class="th">Tipo de combustible</th>
+                                <th class="th">Potencia(CV)</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                                while($fila1 = $resultado1->fetch_assoc()){
+                                    echo "<tr>";
+                                    echo "<td class='td'>$fila1[nombre_modelo]</td>";
+                                    echo "<td class='td'>$fila1[num_puertas]</td>";
+                                    echo "<td class='td'>$fila1[combustible]</td>";
+                                    echo "<td class='td'>$fila1[cv]</td>";
                                     ?>
-                                </tbody>
-                    </table>
-                    <p><a href="modificar-modelo.php">Volver</a></p>
-                </div>
-            <?php
+                                    <!-- Guardo el ID del modelo escogido para llevarlo a la otra página-->
+                                    <td><a href="modificar-modelo2.php?id=<?php echo $fila1['ID_modelo'];?>">Modificar modelo</a></td>
+                                    <?php
+                                    echo "</tr>";
+                                }
+                            ?>
+                        </tbody>
+            </table>
+            <p><a href="modificar-modelo.php">Volver</a></p>
+        </div>
+        <?php
     }
 
     ?>
