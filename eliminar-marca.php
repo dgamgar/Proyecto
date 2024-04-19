3c6420899a1dcea59512d6dd7646f58a314d59d8
daniel <dgamgar755a@ieslacampina.es>
2024-04-19 11:51:53 +0200

    quito eliminar-marca2 pq no sirve y lo meto en eliminar-marca con un if

--- eliminar-marca.php
+++ eliminar-marca.php
@@ -29,6 +29,9 @@ if ($resultado->num_rows > 0) {
         <h1>Eliminar marca</h1>
         <img src="img/logo2.png" class="logo">
     </header>
+    <?php
+    if(!isset($_GET["id"])){
+    ?>
     <div class="container">
         <table id="tabla">
             <thead>
@@ -44,7 +47,7 @@ if ($resultado->num_rows > 0) {
                         echo "<tr>";
                         echo "<td class='td'>$marca</td>";
                         ?>
-                        <td><a href="eliminar-marca2.php?id=<?php echo $marca;?>">Eliminar</a></td>
+                        <td><a href="eliminar-marca.php?id=<?php echo $marca;?>">Eliminar</a></td>
                         <?php
                         echo "</tr>";
                     }
@@ -53,5 +56,40 @@ if ($resultado->num_rows > 0) {
         </table>
         <p><a href="admin.html">Inicio</a></p>
     </div>
+    <?php
+    } else {
+        $nmarca=$_GET["id"];
+
+        //  Sentencia para sacar el ID de la marca
+        $sql="SELECT * FROM marca WHERE nombre_marca='$nmarca'";
+        $resultado=$mysqli->query($sql);
+        
+        // Guardo el ID en una variable
+        while($fila = $resultado->fetch_assoc()){
+            $idmarca=$fila["ID_marca"];
+        }
+
+        // Compruebo si hay modelos de esta marca
+        $sql1="SELECT * FROM modelo WHERE ID_marca='$idmarca'";
+        $resultado1=$mysqli->query($sql1);
+
+        if($resultado1->num_rows>0){
+            // Sí hay modelos
+            ?>
+            <div class="mal">
+                <h2 class="malt">Hay registrados modelos de esta marca, primero elimine los modelos.</h2>
+                <p class="malb"><a href="eliminar-modelo2.php?id=<?php echo $nmarca;?>">Eliminar modelos de esta marca</a></p>
+                <p class="malb"><a href="eliminar-marca.php">Volver</a></p>
+            </div>
+            <?php
+        } else {
+            // No hay modelos
+            // Sentencia DELETE para eliminar la marca escogida
+            $sql2="DELETE FROM marca WHERE ID_marca='$idmarca'";
+            $resultado2=$mysqli->query($sql2);
+            header("Location:eliminar-marca.php");
+        }
+    }
+    ?>
 </body>
 </html>
