be398f90d6fc1ccf3bce68fbc4338d24ff235050
daniel <dgamgar755a@ieslacampina.es>
2024-06-04 12:15:47 +0200

    bootstrap inicio.php

--- inicio.php
+++ inicio.php
@@ -4,63 +4,50 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/inicio.css">
+    <link rel="stylesheet" href="css/estilos.css">
     <link rel="icon" href="img/buyacar_89124.ico">
     <title>Inicio</title>
 </head>
 <body>
     <div class="container">
-
-        <div class="present">
-            <img src="img/logo.png" class="logo">
-            <h1>Bienvenido</h1>
-        </div>
-
-        <div class="texto">
-            <p>En DanCar Autos, nos apasiona poner a disposición de nuestros clientes una amplia gama de vehículos de 
-            la más alta calidad y rendimiento. Con años de experiencia en la industria automotriz, nos enorgullecemos 
-            de ofrecer un servicio excepcional y una selección cuidadosamente curada de automóviles para satisfacer 
-            todas las necesidades y estilos de vida.</p>
-        
-            <p>Nuestro compromiso es proporcionar a nuestros clientes una experiencia de compra de coches sin igual, 
-            donde la calidad, la confianza y la satisfacción del cliente son nuestra máxima prioridad. Desde coches 
-            compactos y eficientes hasta vehículos de lujo y SUVs familiares, tenemos el vehículo perfecto para cada 
-            conductor.</p>
-        
-            <p>En DanCar Autos, no solo vendemos coches, creamos experiencias. Nuestro equipo experto está aquí para 
-            guiarlo en cada paso del proceso, desde la selección del vehículo hasta el financiamiento y el 
-            mantenimiento a largo plazo. Nos esforzamos por superar las expectativas de nuestros clientes en cada 
-            interacción.</p>
+        <div class="card text-center shadow-lg">
+            <div class="card-header bg-info">
+                <div class="row">
+                    <div class="col d-flex justify-content-start">
+                        <h1 style="margin-top: 15px;">Bienvenido</h1>
+                    </div>
+                    <div class="col d-flex justify-content-end">
+                        <img src="img/logo2.png" class="logo">
+                    </div>
+                </div>
+            </div>
+            <div class="card-body">
+                <p>En DanCar Autos, nos apasiona poner a disposición de nuestros clientes una amplia gama de vehículos de 
+                la más alta calidad y rendimiento. Con años de experiencia en la industria automotriz, nos enorgullecemos 
+                de ofrecer un servicio excepcional y una selección cuidadosamente curada de automóviles para satisfacer 
+                todas las necesidades y estilos de vida.</p>
             
-            <p>Explora nuestra amplia selección de coches, y descubre por qué DanCar Autos es tu destino 
-            número uno para la compra de coches. ¡Te damos la bienvenida a la familia DanCar Autos!</p>
-        </div>
-    
-        <div class="botones">
-            <p><a href="registrar.html">Registrarse</a><a href="login.html">Iniciar sesión</a></p>
-        </div>
-        <footer>
-            <div class="rrss">
-                <div class="rrss-item">
-                    <img src="img/telefono.png" class="ico"><p>645868195</p>
-                </div>
-                <div class="rrss-item">
-                    <img src="img/gmail.png" class="ico"><p>dancarautos@gmail.com</p>
-                </div>
-                <div class="rrss-item">
-                    <img src="img/instagram.png" class="ico"><p>@dancar_autos</p>
-                </div>
-            </div>
-            <div class="copy">
-                <div class="copy-item">
-                    <img src="img/copy.png" class="icopy">
-                </div>
-                <div class="copy-item">
-                    <p> 2024 DanCar Autos SL. ALL RIGHTS RESERVED.</p>
-                </div>
-            </div>
-        </footer>
+                <p>Nuestro compromiso es proporcionar a nuestros clientes una experiencia de compra de coches sin igual, 
+                donde la calidad, la confianza y la satisfacción del cliente son nuestra máxima prioridad. Desde coches 
+                compactos y eficientes hasta vehículos de lujo y SUVs familiares, tenemos el vehículo perfecto para cada 
+                conductor.</p>
+            
+                <p>En DanCar Autos, no solo vendemos coches, creamos experiencias. Nuestro equipo experto está aquí para 
+                guiarlo en cada paso del proceso, desde la selección del vehículo hasta el financiamiento y el 
+                mantenimiento a largo plazo. Nos esforzamos por superar las expectativas de nuestros clientes en cada 
+                interacción.</p>
+                
+                <p>Explora nuestra amplia selección de coches, y descubre por qué DanCar Autos es tu destino 
+                número uno para la compra de coches.</p>
 
+                <p>¡Te damos la bienvenida a la familia DanCar Autos!</p>
+            </div>
+            <div class="card-footer d-flex flex-row justify-content-center bg-info">
+                <p style="margin:10px;"><a href="registrar.html" class="btn btn-light">Registrarse</a></p>
+                <p style="margin:10px;"><a href="login.html" class="btn btn-light">Iniciar sesión</a></p>
+            </div>
+        </div>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>
