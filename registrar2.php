90ae2854f0689b2026bc216aee2011659d3ecc21
daniel <dgamgar755a@ieslacampina.es>
2024-06-04 13:43:35 +0200

    bootstrap registrar completo

--- registrar2.php
+++ registrar2.php
@@ -4,22 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/registrar.css">
+    <link rel="stylesheet" href="css/estilos.css">
     <link rel="icon" href="img/buyacar_89124.ico">
     <title>Registrar</title>
 </head>
 <body>
-    <header>
-        <h2>Regístrate</h2>
-        <img src="img/logo2.png" class="logo">
-    </header>
     <?php
     // Me traigo los datos
     $nombre=$_POST["nombre"];
     $fecha_nac=$_POST["fecha_nac"];
     $dni=$_POST["dni"];
     $contraseña=$_POST["contraseña"];
-
 
     // Encripto la contraseña
     $passwd_hashed=password_hash($contraseña, PASSWORD_DEFAULT);
@@ -34,9 +29,16 @@
     if ($resultado->num_rows >0){
         //Ya existe, no se puede crear nuevo usuario
         ?>
-        <div class="mal">
-            <h2 class="malt">Ya existe un usuario con el DNI introducido</h2>
-            <p class="malb"><a href="registrar.html">Volver</a><br><a href="login.html">Iniciar sesión</a></p>
+        <div class="container" style="margin-top:120px;">
+            <div class="card">
+                <div class="card-body bg-danger">
+                    <h5>Ya existe un usuario con el DNI introducido</h5>
+                </div>
+                <div class="card-footer d-flex flex-row justify-content-center">
+                    <p><a href="registrar.html" class="btn btn-warning" style="margin-right:10px;">Volver</a><a href="login.html" class="btn btn-primary">Iniciar sesión</a></p>
+
+                </div>
+            </div>
         </div>
         <?php
     } else {
@@ -44,35 +46,20 @@
         $sql2="INSERT INTO usuarios (rol, nombre, fecha_nac, contraseña, dni) VALUES ('0','$nombre','$fecha_nac','$passwd_hashed','$dni')";
         $resultado2=$mysqli->query($sql2);
         ?>
-        <div class="bien">
-            <br>
-            <h2 class="bi-com">Usuario agregado correctamente</h2>
-            <p><a href="login.html">Iniciar sesión</a></p>
+        <div class="container" style="margin-top:120px;">
+            <div class="card">
+                <div class="card-body bg-success">
+                    <h5>Usuario agregado correctamente</h5>
+                </div>
+                <div class="card-footer d-flex justify-content-center">
+                    <p><a href="login.html" class="btn btn-primary">Iniciar sesión</a></p>
+                </div>
+            </div>
         </div>
         <?php
         
     }
-?>  
-    <footer>
-        <div class="rrss">
-            <div class="rrss-item">
-                <img src="img/telefono.png" class="ico"><p>645868195</p>
-            </div>
-            <div class="rrss-item">
-                <img src="img/gmail.png" class="ico"><p>dancarautos@gmail.com</p>
-            </div>
-            <div class="rrss-item">
-                <img src="img/instagram.png" class="ico"><p>@dancar_autos</p>
-            </div>
-        </div>
-        <div class="copy">
-            <div class="copy-item">
-                <img src="img/copy.png" class="icopy">
-            </div>
-            <div class="copy-item">
-                <p> 2024 DanCar Autos SL. ALL RIGHTS RESERVED.</p>
-            </div>
-        </div>
-    </footer>
+    ?>  
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>
