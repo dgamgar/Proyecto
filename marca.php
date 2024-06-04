996929531a332948b3dd01686660c084f9f267ff
daniel <dgamgar755a@ieslacampina.es>
2024-06-04 16:02:18 +0200

    bootsrap marca,modelo y borro imgs

--- marca.php
+++ marca.php
@@ -24,47 +24,61 @@ if ($resultado->num_rows > 0) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Marca</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/marca.css">
+    <link rel="stylesheet" href="css/estilos.css">
     <link rel="icon" href="img/buyacar_89124.ico">
 </head>
 <body>
     <header>
-        <h1>Escoja la marca que desee</h1>
-        <img src="img/logo2.png">
+        <h2>Escoja la marca que desee</h2>
+        <img src="img/logo2.png" class="logo">
     </header>
     <div class="container">
-                <div class="marcas">
+                <div class="bg-light rounded" style="max-width:80%; padding:10px;">
                     <?php
                     // Recorro todo el array mostrando las marcas que hay almacenadas
                     foreach ($marcas as $marca){
                         ?>
-                        <a href="modelo.php?id=<?php echo $marca;?>"><img src="img/<?php echo $marca;?>.png"></a>
+                        <a href="modelo.php?id=<?php echo $marca;?>"><img src="img/<?php echo $marca;?>.png" style="width:120px; padding:10px;"></a>
                         <?php
                     }
                     ?>
                 </div>
-        <p><a href="clientes.html" class="boton">Volver</a></p>
+                <br>
+        <p><a href="clientes.html" class="btn btn-warning">Volver</a></p>
     </div>
-    <footer>
-        <div class="rrss">
-            <div class="rrss-item">
-                <img src="img/telefono.png" class="ico"><p>645868195</p>
+    <footer class="bg-info">
+        <div class="row">
+            <p>Contáctanos</p>
+            <span>------------------------------</span>
+            <div class="row">
+                <div class="col-sm-2">
+                    <p>685489625</p>
+                </div>
+                <div class="col-sm-1">
+                    <img src="img/telefono.png" style="width:30px;">
+                </div>
             </div>
-            <div class="rrss-item">
-                <img src="img/gmail.png" class="ico"><p>dancarautos@gmail.com</p>
+            <div class="row">
+                <div class="col-sm-2">
+                    <p>dancarautos@gamil.com</p>
+                </div>
+                <div class="col-sm-1">
+                    <img src="img/gmail.png" style="width:30px;">
+                </div>
             </div>
-            <div class="rrss-item">
-                <img src="img/instagram.png" class="ico"><p>@dancar_autos</p>
+            <div class="row">
+                <div class="col-sm-2">
+                    <p>@dancar_autos</p>
+                </div>
+                <div class="col-sm-1">
+                    <img src="img/instagram.png" style="width:30px;">
+                </div>
             </div>
         </div>
-        <div class="copy">
-            <div class="copy-item">
-                <img src="img/copy.png" class="icopy">
-            </div>
-            <div class="copy-item">
-                <p> 2024 DanCar Autos SL. ALL RIGHTS RESERVED.</p>
-            </div>
+        <div class="text-center">
+            <p><img src="img/copy.png" class="icopy" style="width:20px;"> 2024 DanCar Autos SL. ALL RIGHTS RESERVED.</p>
         </div>
     </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>
--- modelo.php
+++ modelo.php
@@ -25,7 +25,7 @@ $resultado1=$mysqli->query($sql1);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Modelos</title>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/modelo.css">
+    <link rel="stylesheet" href="css/estilos.css">
 	<link rel="icon" href="img/buyacar_89124.ico">
 </head>
 <body>
@@ -37,68 +37,91 @@ $resultado1=$mysqli->query($sql1);
 	// Compruebo si hay modelos asociados a la marca escogida
 	if($resultado1->num_rows>0){
 		// Sí los hay
-	?>
-	<div class="container">
-		<!-- Tabla con los modelos -->
-		<table id="tabla" class="display" >
-			<thead>
-				<tr>
-					<th class="th">Modelo</th>
-					<th class="th">Número de puertas</th>
-					<th class="th">Tipo de combustible</th>
-					<th class="th">Potencia(CV)</th>
-					<th></th>
-				</tr>
-			</thead>
-			<tbody>
-				<?php
-					while($fila1 = $resultado1->fetch_assoc()){
-						echo "<tr>";
-						echo "<td class='td'>$fila1[nombre_modelo]</td>";
-						echo "<td class='td'>$fila1[num_puertas]</td>";
-						echo "<td class='td'>$fila1[combustible]</td>";
-						echo "<td class='td'>$fila1[cv]</td>";
-						?>
-						<!-- Guardo el ID del modelo escogido para llevarlo a la otra página-->
-						<td><a href="vehiculo.php?id=<?php echo $fila1['ID_modelo'];?>">Ver vehículos disponibles</a></td>
-						<?php
-						echo "</tr>";
-					}
-				?>
-			</tbody>
-		</table>
-	</div>
+		?>
+		<div class="container">
+			<!-- Tabla con los modelos -->
+			<table id="tabla" class="display" >
+				<thead>
+					<tr class="bg-dark">
+						<th style="color:white; padding:10px;">Modelo</th>
+						<th style="color:white; padding:10px;">Número de puertas</th>
+						<th style="color:white; padding:10px;">Tipo de combustible</th>
+						<th style="color:white; padding:10px;">Potencia(CV)</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php
+						while($fila1 = $resultado1->fetch_assoc()){
+							echo "<tr>";
+							echo "<td class='bg-light border-bottom' style='padding:10px;'>$fila1[nombre_modelo]</td>";
+							echo "<td class='bg-light border-bottom' style='padding:10px;'>$fila1[num_puertas]</td>";
+							echo "<td class='bg-light border-bottom' style='padding:10px;'>$fila1[combustible]</td>";
+							echo "<td class='bg-light border-bottom' style='padding:10px;'>$fila1[cv]</td>";
+							?>
+							<!-- Guardo el ID del modelo escogido para llevarlo a la otra página-->
+							<td><a href="vehiculo.php?id=<?php echo $fila1['ID_modelo'];?>" class="btn btn-primary">Ver vehículos disponibles</a></td>
+							<?php
+							echo "</tr>";
+						}
+					?>
+				</tbody>
+			</table>
+			<br>
+			<p><a href="marca.php" class="btn btn-warning">Volver</a></p>
+		</div>
 		<?php
 	} else {
 		// No los hay
 		?>
-		<div class="nohay">
-		<h2>Lo sentimos, en estos momentos no tenemos modelos disponibles de esa marca.</h2>
+		<div class="container">
+			<div class="card">
+				<div class="card-header bg-danger rounded">
+					<h2 style="padding:10px;">ERROR:No hay modelos</h2>
+				</div>
+				<div class="card-body bg-light">
+					<p>Lo sentimos, no tenemos modelos disponibles de la marca escogida, inténtelo de nuevo con otra marca</p>
+				</div>
+			</div>
+			<br>
+			<p><a href="marca.php" class="btn btn-warning">Volver</a></p>
 		</div>
 	<?php
 	}
 	?>
-	<p><a href="marca.php">Volver</a></p>
-	<footer>
-		<div class="rrss">
-			<div class="rrss-item">
-				<img src="img/telefono.png" class="ico"><p>645868195</p>
-			</div>
-			<div class="rrss-item">
-				<img src="img/gmail.png" class="ico"><p>dancarautos@gmail.com</p>
-			</div>
-			<div class="rrss-item">
-				<img src="img/instagram.png" class="ico"><p>@dancar_autos</p>
-			</div>
-		</div>
-		<div class="copy">
-			<div class="copy-item">
-				<img src="img/copy.png" class="icopy">
-			</div>
-			<div class="copy-item">
-				<p> 2024 DanCar Autos SL. ALL RIGHTS RESERVED.</p>
-			</div>
-		</div>
-	</footer>
+
+	<footer class="fixed-bottom">
+        <div class="row">
+            <p>Contáctanos</p>
+            <span>------------------------------</span>
+            <div class="row">
+                <div class="col-sm-2">
+                    <p>685489625</p>
+                </div>
+                <div class="col-sm-1">
+                    <img src="img/telefono.png" style="width:30px;">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-2">
+                    <p>dancarautos@gamil.com</p>
+                </div>
+                <div class="col-sm-1">
+                    <img src="img/gmail.png" style="width:30px;">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-2">
+                    <p>@dancar_autos</p>
+                </div>
+                <div class="col-sm-1">
+                    <img src="img/instagram.png" style="width:30px;">
+                </div>
+            </div>
+        </div>
+        <div class="text-center">
+            <p><img src="img/copy.png" class="icopy" style="width:20px;"> 2024 DanCar Autos SL. ALL RIGHTS RESERVED.</p>
+        </div>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>
